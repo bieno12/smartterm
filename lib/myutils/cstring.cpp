51fd92c9025fd2c996f11ce676fcf10301816074
Zeyad Shahin <zeyadshahin2015@gmail.com>
2025-11-20 11:16:20 +0200

    update task4 to be more readable

--- lib/myutils/cstring.cpp
+++ lib/myutils/cstring.cpp
@@ -3,9 +3,10 @@
 uint32_t myutils::strLen(const char *str)
 {
     uint32_t i = 0;
-    while (str[i++]);
+    while (str[i] != '\0')
+        i++;
 
-    return i - 1;
+    return i;
 }
 
 
@@ -15,17 +16,17 @@ uint32_t myutils::strLen(const char *str)
 /// @return returns +ve if s1 > s2, 0 if s1 == s2, -ve s1 < s2
 int32_t myutils::strCompare(const char *s1,const char *s2)
 {
-    while(*s1 && *s2 && *s1 == *s2)
+    while(*s1 != '\0' && *s2 != '\0' && *s1 == *s2)
     {
         s1++; s2++;
     };
 
-    if (!*s1 && !*s2)
+    if (*s1 == '\0' && *s2 == '\0')
         return 0;
-    if(!*s1)
+    if(*s1 == '\0')
         return -1;
 
-    if(!*s2)
+    if(*s2 == '\0')
         return 1;
     
     return (*s1 - *s2);
@@ -33,7 +34,7 @@ int32_t myutils::strCompare(const char *s1,const char *s2)
 
 void myutils::strToLower(char *str)
 {
-    while(*str)
+    while(*str != '\0')
     {
         if (*str >= 'A' && *str <= 'Z')
             *str += 'a' - 'A';
@@ -43,7 +44,7 @@ void myutils::strToLower(char *str)
 
 void myutils::strToUpper(char *str)
 {
-    while(*str)
+    while(*str != '\0')
     {
         if (*str >= 'a' && *str <= 'z')
             *str += 'A' - 'a';
@@ -53,16 +54,17 @@ void myutils::strToUpper(char *str)
 void myutils::strConcat(uint32_t sizeStr1, char *str1, const char *str2)
 {
 
-    while(*str1)
+    while(*str1 != '\0')
     {
         str1++;
         sizeStr1--;
     }
-    while(*str2)
+    while(*str2 != '\0')
     {
         if(sizeStr1 == 0)
             return ;
-        *str1++ = *str2++;
+        *str1 = *str2;
+        str1++, str2++;
         sizeStr1--; 
     }
     *str1 = '\0';
@@ -70,7 +72,10 @@ void myutils::strConcat(uint32_t sizeStr1, char *str1, const char *str2)
 
 void myutils::strCopy(char *dest, const char *src)
 {
-    while(*src)
-        *dest++ = *src++;
+    while(*src != '\0')
+    {
+        *dest = *src;
+        dest++, src++;
+    }
     *dest = '\0';
 }
