f57d97087e14ad0920293580f9af23af8815dac2
Zeyad Shahin <zeyadshahin2015@gmail.com>
2025-11-26 16:49:09 +0200

    clean up

--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,11 @@
 #include <term/term.h>
 #include <term/input.h>
 #include <term/tui.h>
-#include <term/signal.h>
-#include "application.h"
 using namespace std;
-
-
 
 int main(int, char**){
     term::clearScreen();
     term::input::setInputMode(term::input::InputMode::Raw);
     term::hideCursor();
-
-    Application app;
-
-    app.run();
-
+    
 }
